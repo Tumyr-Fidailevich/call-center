b1e0b3047bed119f8562e98a2a6e82623b093d74
Timur <timurkashapov15@gmail.com>
2023-11-28 11:16:29 +0300

    added precompile headers

---
+++ include/pch.h
@@ -0,0 +1,22 @@
+#ifndef CALL_CENTER_PCH_H
+#define CALL_CENTER_PCH_H
+
+#ifndef GLOG_NO_ABBREVIATED_SEVERITIES
+#define GLOG_NO_ABBREVIATED_SEVERITIES
+#endif
+
+#include <iostream>
+#include <queue>
+#include <mutex>
+#include <thread>
+#include <memory>
+#include "boost/filesystem.hpp"
+#include "boost/filesystem/fstream.hpp"
+#include "boost/asio.hpp"
+#include "boost/smart_ptr.hpp"
+#include "boost/date_time/posix_time/posix_time.hpp"
+#include "boost/asio/awaitable.hpp"
+#include "nlohmann/json.hpp"
+#include "glog/logging.h"
+
+#endif
