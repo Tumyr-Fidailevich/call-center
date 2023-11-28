6868cb3c745f811c40419531a7b1e2f7574e37cc
Timur <timurkashapov15@gmail.com>
2023-11-28 11:27:00 +0300

    add CDR impl

---
+++ include/cdr.h
@@ -0,0 +1,27 @@
+#ifndef CALL_CENTER_CDR_H
+#define CALL_CENTER_CDR_H
+
+#include "pch.h"
+
+using namespace boost::posix_time;
+
+struct CDR
+{
+	ptime connectionTime;
+	ptime responseTime;
+	ptime disconnectTime;
+	int callDuration{0};
+	int operatorId{0};
+	std::string callId{};
+	std::string phoneNumber{};
+	std::string status{};
+
+	void write();
+};
+
+void CDR::write()
+{
+
+}
+
+#endif
