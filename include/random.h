1f4ae2ec1954a04fc0e6c240922c16ddf178f483
Timur <timurkashapov15@gmail.com>
2023-11-29 22:35:52 +0300

    added random function to get random call duration

---
+++ include/random.h
@@ -0,0 +1,16 @@
+#ifndef CALL_CENTER_RANDOM_H
+#define CALL_CENTER_RANDOM_H
+
+#include "pch.h"
+
+
+
+std::chrono::seconds getRandomCallDuration(const std::chrono::seconds &min, const std::chrono::seconds &max) {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<std::chrono::seconds::rep> dist(min.count(), max.count());
+
+	return std::chrono::seconds(dist(gen));
+}
+
+#endif
