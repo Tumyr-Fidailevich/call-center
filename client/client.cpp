b15412cc85f4e57edac8a613da87a9968838d9f2
Timur <timurkashapov15@gmail.com>
2023-12-07 16:14:00 +0300

    add getRandomPhoneNumber func in client class

--- client/client.cpp
+++ client/client.cpp
@@ -1,43 +1,73 @@
-#include <boost/asio.hpp>
-
-#include <array>
+#include "boost/asio.hpp"
+#include <random>
 #include <iostream>
 #include <string>
+#include <memory>
+#include "glog/logging.h"
 
-namespace ba = boost::asio;
 
-class AsyncClient {
+using namespace boost::asio;
+using ip::tcp;
+
+
+
+long long getRandomNumber(long long min, long long max) {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<long long> dis(min, max);
+	return dis(gen);
+}
+
+std::string getRandomPhoneNumber() {
+
+	auto phoneNumber = getRandomNumber(70000000000LL, 89999999999LL);
+
+	std::ostringstream oss;
+	oss << "+" << phoneNumber;
+
+	return oss.str();
+}
+
+
+class AsyncClient
+{
 public:
-	AsyncClient(ba::io_context& io_context)
-			: m_tcp_socket{io_context}
-			, m_resolver{io_context} {
-
+	AsyncClient(io_context &io_context, std::string _number)
+			: socket{io_context}, resolver{io_context}, number{std::move(_number)}
+	{
+		data.resize(max_length, '\0');
 	}
 
-	void get(const std::string& host, const std::string& service) {
+	void get(const std::string &host, const std::string &service)
+	{
 
-		ba::ip::tcp::resolver::query query{host, service};
-		m_resolver.async_resolve(
+		tcp::resolver::query query{host, service};
+		resolver.async_resolve(
 				query,
-				[&](const boost::system::error_code &ec, ba::ip::tcp::resolver::iterator it) {
+				[&](const boost::system::error_code &ec, const tcp::resolver::iterator &it) {
 					this->resolveHandler(ec, it);
 				}
 		);
 	}
 
 private:
-	void resolveHandler(const boost::system::error_code &ec, ba::ip::tcp::resolver::iterator it) {
-		if (ec) {
-			std::cout << "resolveHandler - failed! err = " << ec.message() << std::endl;
+	void resolveHandler(const boost::system::error_code &ec, const ba::ip::tcp::resolver::iterator &it)
+	{
+		if (ec)
+		{
+			std::cout << "Resolve handler for user with phone number " << number << " - failed: " << ec.what()
+					  << std::endl;
 			return;
 		}
 
-		if (it == ba::ip::tcp::resolver::iterator{}) {
-			std::cout << "resolveHandler - no address!" << std::endl;
+		if (it == tcp::resolver::iterator{})
+		{
+			std::cout << "Resolve handler for user with phone number " << number
+					  << " - no address: " << std::endl;
 			return;
 		}
 
-		m_tcp_socket.async_connect(
+		socket.async_connect(
 				*it,
 				[&](const boost::system::error_code &ec) {
 					this->connectHandler(ec);
@@ -45,68 +75,64 @@ private:
 		);
 	}
 
-	void connectHandler(const boost::system::error_code &ec) {
-		if (ec) {
-			std::cout << "connectHandler - failed! err = " << ec.message() << std::endl;
+	void connectHandler(const boost::system::error_code &ec)
+	{
+		if (ec)
+		{
+			std::cout << "Connect handler for user with phone number " << number
+					  << " - failed: " << ec.what() << std::endl;
 			return;
 		}
 
-		const std::string request = "GET /notExists HTTP/1.1\r\n\r\n";
-		ba::write(m_tcp_socket, ba::buffer(request));
+		const std::string request = "GET /call-center?phone=" + number + " HTTP/1.1\r\n\r\n";
+		write(socket, buffer(request));
 
-		m_tcp_socket.async_read_some(
-				ba::buffer(m_buffer),
-				[&](const boost::system::error_code &ec, std::size_t bytes_transferred) {
-					this->readHandler(ec, bytes_transferred);
+		socket.async_read_some(
+				buffer(data, max_length),
+				[&](const boost::system::error_code &ec, std::size_t length) {
+					this->readHandler(ec, length);
 				}
 		);
 	}
 
-	void readHandler(const boost::system::error_code &ec, std::size_t bytes_transferred) {
-		if (ec) {
-			std::cout << "readHandler - failed! err = " << ec.message() << std::endl;
+	void readHandler(const boost::system::error_code &ec, std::size_t length)
+	{
+		if (ec)
+		{
+			std::cout << "Read handler for user with phone number " << number
+					  << " - failed: " << ec.what() << std::endl;
 			return;
 		}
 
-		if (!bytes_transferred) {
-			std::cout << "readHandler - no more data!" << std::endl;
+		if (!length)
+		{
+			std::cout << "Read handler for user with phone number " << number
+					  << " - no more data: " << ec.what() << std::endl;
 			return;
 		}
-
-		std::cout << "[readHandler] - read " << bytes_transferred << " bytes" << std::endl;
-
-		std::cout.write(m_buffer.data(), bytes_transferred);
-		m_tcp_socket.async_read_some(
-				ba::buffer(m_buffer),
-				[&](const boost::system::error_code &ec, std::size_t bytes_transferred) {
-					readHandler(ec, bytes_transferred);
-				}
-		);
+		std::cout << data;
 	}
 
-	ba::ip::tcp::resolver m_resolver;
-	ba::ip::tcp::socket m_tcp_socket;
-	std::array<char, 4096> m_buffer;
-
+	tcp::resolver resolver;
+	tcp::socket socket;
+	enum{
+		max_length = 1024
+	};
+	std::string data;
+	std::string number;
 };
 
-int main() {
+int main()
+{
 	std::locale::global(std::locale(""));
 
-	ba::io_context io_context;
+	io_context ioContext;
 
-	AsyncClient client{ io_context };
+	AsyncClient client(ioContext, getRandomPhoneNumber());
 
 	client.get("127.0.0.1", "1234");
 
-	io_context.run();
-
-	std::cout << "After the io_context.run() " << std::endl;
+	ioContext.run();
 
 	return 0;
 }
-
-
-
-
-
