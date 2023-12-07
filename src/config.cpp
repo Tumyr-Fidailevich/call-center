98fd1236ff63b1abe170473be1c75ae21c1fa774
Timur <timurkashapov15@gmail.com>
2023-12-07 14:40:49 +0300

    change logging in config

--- src/config.cpp
+++ src/config.cpp
@@ -1,5 +1,4 @@
 #include "config.h"
-#include "logging_macros.h"
 
 
 Config::Config(std::string &filename) : configFile(std::move(filename))
@@ -13,13 +12,13 @@ void Config::update()
 	if (!boost::filesystem::exists(configFile))
 	{
 		setDefault();
-		LOG_TO_FILE(google::GLOG_WARNING, LOG_FILE) << "Could not find the current config";
+		LOG(WARNING) << "Could not find current config path";
 		return;
 	}
 	boost::filesystem::ifstream config(configFile);
 	if (!config.is_open())
 	{
-		LOG_TO_FILE(google::GLOG_WARNING, LOG_FILE) << "Could not open the current config";
+		LOG(WARNING) << "Could not open current config";
 		setDefault();
 		return;
 	}
@@ -32,7 +31,7 @@ void Config::update()
 	minCallDuration = std::chrono::seconds(jsonConfig.value("min_call_duration", 20));
 	maxCallDuration = std::chrono::seconds(jsonConfig.value("max_call_duration", 40));
 	numOfOperators = jsonConfig.value("number_of_operators", 5);
-	LOG_TO_FILE(google::GLOG_INFO, LOG_FILE) << "Config updated successfully";
+	LOG(INFO) << "Config updated successfully";
 }
 
 
@@ -43,6 +42,5 @@ void Config::setDefault()
 	minCallDuration = std::chrono::seconds(20);
 	maxCallDuration = std::chrono::seconds(40);
 	numOfOperators = 2;
-	LOG_TO_FILE(google::GLOG_INFO, LOG_FILE) << "Default config installed successfully";
-
+	LOG(INFO) << "Default config installed successfully";
 }
